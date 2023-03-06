35667807fa393b0a8e97f7b2f1dbc3302de54937
goodcheese92 <dltjdwls555@gmail.com>
2023-03-06 15:53:10 +0900

    asdasd

--- public/MenuJS.js
+++ public/MenuJS.js
@@ -3,11 +3,11 @@ $(function (){
     // upperMenu 동작 감지
     $(".date").click(function (){
         let Check = $(this).parent().find(".subMenu").is(":visible");
-        if(Check){
-            alert("참");
-        } else{
-            alert("거짓");
-        }
+        // if(Check){
+        //     alert("참");
+        // } else{
+        //     alert("거짓");
+        // }
         let dateHtml = $(this).html();
         // alert(dateHtml);
         if(Check){
@@ -20,11 +20,11 @@ $(function (){
 let exCheck = false;
     $(".exMenu").click(function (){
         let Check = $(this).parent().find(".contents").is(":visible");
-        if(Check){
-          alert("참");
-        } else{
-          alert("거짓");
-        }
+        // if(Check){
+        //   alert("참");
+        // } else{
+        //   alert("거짓");
+        // }
         if(Check){
             $(this).parent().find(".contents").css("display", "none");
         } else {
