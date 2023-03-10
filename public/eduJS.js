06b63102a743a6c571ebecca68463fab46198af7
goodcheese92 <dltjdwls555@gmail.com>
2023-03-10 15:46:54 +0900

    2023-03-10 오후

---
+++ public/eduJS.js
@@ -0,0 +1,11 @@
+// 0227_ex03
+// let html = document.#li_0227_ex03.innerHTML;
+// html = "test";
+// let name = "hong";
+let h1 = document.createElement("h1");
+li_0227_ex03.innerHTML = "test";
+// li_0227_ex03.appendChild(h1);
+function ul_0227_ex03(){
+    let li_0227_ex03 = document.getElementById("li_0227_ex03");
+    li_0227_ex03.innerHTML = 'test';
+} // end of ul_0227_ex03
